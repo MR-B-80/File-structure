7fe00f636f7f0a5088fb80a14ebb58fa8e9d7fac
MR-B-80 <88060670+MR-B-80@users.noreply.github.com>
2022-02-19 14:20:22 +0330

    .

--- js/mainFunctions.js
+++ js/mainFunctions.js
@@ -1,5 +1,6 @@
 $(document).ready(() => {
     $('.navbar_wraper').load('/components/navbar.html');    
     $('.footer_wraper').load('/components/footer.html');
+    $(".mobile").load("/components/mobile.html");
     
 });
